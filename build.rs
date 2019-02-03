1d7a3f4bb5b4f29754dd7fc062f18323b525937c
LongYinan <lynweklm@gmail.com>
2019-02-03 16:46:44 +0800

    Fix windows build

--- build.rs
+++ build.rs
@@ -37,9 +37,13 @@ fn main() {
     r#"--args=is_official_build=true skia_use_system_expat=false skia_use_system_icu=false skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_zlib=false cc="clang" cxx="clang++""#
   };
 
-  println!("{}", gn_arg);
+  let gn_command = if cfg!(windows) {
+    "skia/bin/gn"
+  } else {
+    "bin/gn"
+  };
 
-  let output = Command::new("bin/gn")
+  let output = Command::new(gn_command)
     .args(&[
       "gen",
       "out/Static",
