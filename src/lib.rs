029caee2c582467131e76b5f8457bc83bedc2cfd
Armin Sander <armin@replicator.org>
2019-02-05 14:38:26 +0100

    export all bindings (preparation to migrate this repo to skia-raw)

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+mod bindings;
 mod canvas;
 
-pub use self::canvas::*;
+pub use canvas::Canvas;
+
+pub use self::bindings::*;
