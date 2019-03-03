83f54d2d935725b651b190afe31b805b3c54f820
Armin Sander <armin@replicator.org>
2019-03-03 19:55:52 +0100

    Paint: support method chaining

--- src/skia/paint.rs
+++ src/skia/paint.rs
@@ -120,12 +120,14 @@ impl Default for Handle<SkPaint> {
 
 impl Handle<SkPaint> {
 
-    pub fn reset(&mut self) {
+    pub fn reset(&mut self) -> &mut Self {
         unsafe { self.native_mut().reset() }
+        self
     }
 
-    pub fn set_hinting(&mut self, hinting_level: FontHinting) {
+    pub fn set_hinting(&mut self, hinting_level: FontHinting) -> &mut Self {
         unsafe { self.native_mut().setHinting(hinting_level.native()) }
+        self
     }
 
     pub fn hinting(&self) -> FontHinting {
@@ -139,24 +141,27 @@ impl Handle<SkPaint> {
         })
     }
 
-    pub fn set_flags(&mut self, flags: PaintFlags) {
+    pub fn set_flags(&mut self, flags: PaintFlags) -> &mut Self {
         unsafe { self.native_mut().setFlags(flags.bits()) }
+        self
     }
 
     pub fn anti_alias(&self) -> bool {
         unsafe { self.native().isAntiAlias() }
     }
 
-    pub fn set_anti_alias(&mut self, anti_alias: bool) {
+    pub fn set_anti_alias(&mut self, anti_alias: bool) -> &mut Self {
         unsafe { self.native_mut().setAntiAlias(anti_alias) }
+        self
     }
 
     pub fn dither(&self) -> bool {
         unsafe { self.native().isDither() }
     }
 
-    pub fn set_dither(&mut self, dither: bool) {
+    pub fn set_dither(&mut self, dither: bool) -> &mut Self {
         unsafe { self.native_mut().setDither(dither) }
+        self
     }
 
     pub fn linear_text(&self) -> bool {
@@ -165,8 +170,9 @@ impl Handle<SkPaint> {
         self.flags().contains(PaintFlags::LinearText)
     }
 
-    pub fn set_linear_text(&mut self, linear_text: bool) {
+    pub fn set_linear_text(&mut self, linear_text: bool) -> &mut Self {
         unsafe { self.native_mut().setLinearText(linear_text) }
+        self
     }
 
     pub fn subpixel_text(&self) -> bool {
@@ -175,8 +181,9 @@ impl Handle<SkPaint> {
         self.flags().contains(PaintFlags::SubpixelText)
     }
 
-    pub fn set_subpixel_text(&mut self, subpixel_text: bool) {
+    pub fn set_subpixel_text(&mut self, subpixel_text: bool) -> &mut Self {
         unsafe { self.native_mut().setSubpixelText(subpixel_text) }
+        self
     }
 
     pub fn lcd_render_text(&self) -> bool {
@@ -185,8 +192,9 @@ impl Handle<SkPaint> {
         self.flags().contains(PaintFlags::LCDRenderText)
     }
 
-    pub fn set_lcd_render_text(&mut self, lcd_text: bool) {
+    pub fn set_lcd_render_text(&mut self, lcd_text: bool) -> &mut Self {
         unsafe { self.native_mut().setLCDRenderText(lcd_text) }
+        self
     }
 
     pub fn embedded_bitmap_text(&self) -> bool {
@@ -195,8 +203,9 @@ impl Handle<SkPaint> {
         self.flags().contains(PaintFlags::EmbeddedBitmapText)
     }
 
-    pub fn set_embedded_bitmap_text(&mut self, use_embedded_bitmap_text: bool) {
+    pub fn set_embedded_bitmap_text(&mut self, use_embedded_bitmap_text: bool) -> &mut Self {
         unsafe { self.native_mut().setEmbeddedBitmapText(use_embedded_bitmap_text) }
+        self
     }
 
     pub fn autohinted(&self) -> bool {
@@ -205,8 +214,9 @@ impl Handle<SkPaint> {
         self.flags().contains(PaintFlags::AutoHinting)
     }
 
-    pub fn set_autohinted(&mut self, use_autohinter: bool) {
+    pub fn set_autohinted(&mut self, use_autohinter: bool) -> &mut Self {
         unsafe { self.native_mut().setAutohinted(use_autohinter) }
+        self
     }
 
     pub fn fake_bold_text(&self) -> bool {
@@ -215,8 +225,9 @@ impl Handle<SkPaint> {
         self.flags().contains(PaintFlags::FakeBoldText)
     }
 
-    pub fn set_fake_bold_text(&mut self, fake_bold_text: bool) {
+    pub fn set_fake_bold_text(&mut self, fake_bold_text: bool) -> &mut Self {
         unsafe { self.native_mut().setFakeBoldText(fake_bold_text) }
+        self
     }
 
     pub fn filter_quality(&self) -> FilterQuality {
@@ -224,16 +235,18 @@ impl Handle<SkPaint> {
             .into_handle()
     }
 
-    pub fn set_filter_quality(&mut self, quality: FilterQuality) {
+    pub fn set_filter_quality(&mut self, quality: FilterQuality) -> &mut Self {
         unsafe { self.native_mut().setFilterQuality(quality.native()) }
+        self
     }
 
     pub fn style(&self) -> PaintStyle {
         unsafe { self.native().getStyle() }.into_handle()
     }
 
-    pub fn set_style(&mut self, style: PaintStyle) {
+    pub fn set_style(&mut self, style: PaintStyle) -> &mut Self {
         unsafe { self.native_mut().setStyle(style.native()) }
+        self
     }
 
     pub fn color(&self) -> Color {
@@ -244,45 +257,51 @@ impl Handle<SkPaint> {
         Color4f::from_native(unsafe { self.native().getColor4f() })
     }
 
-    pub fn set_color(&mut self, color: Color) {
+    pub fn set_color(&mut self, color: Color) -> &mut Self {
         unsafe { self.native_mut().setColor(color.into_native()) }
+        self
     }
 
     // TODO: why is ColorSpace mutable?
-    pub fn set_color4f(&mut self, color: Color4f, color_space: &mut ColorSpace) {
+    pub fn set_color4f(&mut self, color: Color4f, color_space: &mut ColorSpace) -> &mut Self {
         unsafe {
             self.native_mut().setColor4f(
                 &color.into_native(),
                 color_space.native_mut() )
         }
+        self
     }
 
     pub fn alpha(&self) -> u8 {
         unsafe { self.native().getAlpha() }
     }
 
-    pub fn set_alpha(&mut self, alpha: u8) {
+    pub fn set_alpha(&mut self, alpha: u8) -> &mut Self {
         unsafe { self.native_mut().setAlpha(alpha as _) }
+        self
     }
 
-    pub fn set_argb(&mut self, a: u8, r: u8, g: u8, b: u8) {
+    pub fn set_argb(&mut self, a: u8, r: u8, g: u8, b: u8) -> &mut Self {
         unsafe { self.native_mut().setARGB(a as _, r as _, g as _, b as _)}
+        self
     }
 
     pub fn stroke_width(&self) -> scalar {
         unsafe { self.native().getStrokeWidth() }
     }
 
-    pub fn set_stroke_width(&mut self, width: scalar) {
+    pub fn set_stroke_width(&mut self, width: scalar) -> &mut Self {
         unsafe { self.native_mut().setStrokeWidth(width) }
+        self
     }
 
     pub fn stroke_miter(&self) -> scalar {
         unsafe { self.native().getStrokeMiter() }
     }
 
-    pub fn set_stroke_miter(&mut self, miter: scalar) {
+    pub fn set_stroke_miter(&mut self, miter: scalar) -> &mut Self {
         unsafe { self.native_mut().setStrokeMiter(miter) }
+        self
     }
 
     pub fn stroke_cap(&self) -> PaintCap {
@@ -290,8 +309,9 @@ impl Handle<SkPaint> {
             .into_handle()
     }
 
-    pub fn set_stroke_cap(&mut self, cap: PaintCap) {
+    pub fn set_stroke_cap(&mut self, cap: PaintCap) -> &mut Self {
         unsafe { self.native_mut().setStrokeCap(cap.native()) }
+        self
     }
 
     pub fn stroke_join(&self) -> PaintJoin {
@@ -299,8 +319,9 @@ impl Handle<SkPaint> {
             .into_handle()
     }
 
-    pub fn set_stroke_join(&mut self, join: PaintJoin) {
+    pub fn set_stroke_join(&mut self, join: PaintJoin) -> &mut Self {
         unsafe { self.native_mut().setStrokeJoin(join.native()) }
+        self
     }
 
     pub fn fill_path(&self, src: &Path, cull_rect: Option<&Rect>, res_scale: Option<scalar>) -> Option<Path> {
@@ -327,10 +348,11 @@ impl Handle<SkPaint> {
     }
 
 
-    pub fn set_color_filter(&mut self, color_filter: Option<&ColorFilter>) {
+    pub fn set_color_filter(&mut self, color_filter: Option<&ColorFilter>) -> &mut Self {
         unsafe {
             C_SkPaint_setColorFilter(self.native_mut(), color_filter.shared_ptr())
         }
+        self
     }
 
     pub fn blend_mode(&self) -> BlendMode {
@@ -345,10 +367,11 @@ impl Handle<SkPaint> {
         }
     }
 
-    pub fn set_blend_mode(&mut self, mode: BlendMode) {
+    pub fn set_blend_mode(&mut self, mode: BlendMode) -> &mut Self {
         unsafe {
             self.native_mut().setBlendMode(mode.native())
         }
+        self
     }
 
     pub fn path_effect(&self) -> Option<PathEffect> {
@@ -357,10 +380,11 @@ impl Handle<SkPaint> {
         })
     }
 
-    pub fn set_path_effect(&mut self, path_effect: Option<&PathEffect>) {
+    pub fn set_path_effect(&mut self, path_effect: Option<&PathEffect>) -> &mut Self {
         unsafe {
             C_SkPaint_setPathEffect(self.native_mut(), path_effect.shared_ptr())
         }
+        self
     }
 
     pub fn mask_filter(&self) -> Option<MaskFilter> {
@@ -369,10 +393,11 @@ impl Handle<SkPaint> {
         })
     }
 
-    pub fn set_mask_filter(&mut self, mask_filter: Option<&MaskFilter>) {
+    pub fn set_mask_filter(&mut self, mask_filter: Option<&MaskFilter>) -> &mut Self {
         unsafe {
             C_SkPaint_setMaskFilter(self.native_mut(), mask_filter.shared_ptr())
         }
+        self
     }
 
     pub fn typeface(&self) -> Option<Typeface> {
@@ -381,10 +406,11 @@ impl Handle<SkPaint> {
         })
     }
 
-    pub fn set_typeface(&mut self, typeface: Option<&Typeface>) {
+    pub fn set_typeface(&mut self, typeface: Option<&Typeface>) -> &mut Self {
         unsafe {
             C_SkPaint_setTypeface(self.native_mut(), typeface.shared_ptr())
         }
+        self
     }
 
     /* TODO: ImageFilter postponed
@@ -395,10 +421,11 @@ impl Handle<SkPaint> {
         })
     }
 
-    pub fn set_image_filter(&mut self, image_filter: Option<&ImageFilter>) {
+    pub fn set_image_filter(&mut self, image_filter: Option<&ImageFilter>) -> &mut Self {
         unsafe {
             C_SkPaint_setImageFilter(self.native_mut(), image_filter.shared_ptr())
         }
+        self
     }
 
     */
@@ -409,24 +436,27 @@ impl Handle<SkPaint> {
         unsafe { self.native().getTextSize() }
     }
 
-    pub fn set_text_size(&mut self, text_size: scalar) {
+    pub fn set_text_size(&mut self, text_size: scalar) -> &mut Self {
         unsafe { self.native_mut().setTextSize(text_size) }
+        self
     }
 
     pub fn text_scale_x(&self) -> scalar {
         unsafe { self.native().getTextScaleX() }
     }
 
-    pub fn set_text_scale_x(&mut self, scale_x: scalar) {
+    pub fn set_text_scale_x(&mut self, scale_x: scalar) -> &mut Self {
         unsafe { self.native_mut().setTextScaleX(scale_x) }
+        self
     }
 
     pub fn text_skew_x(&self) -> scalar {
         unsafe { self.native().getTextSkewX() }
     }
 
-    pub fn set_text_skew_x(&mut self, skew_x: scalar) {
+    pub fn set_text_skew_x(&mut self, skew_x: scalar) -> &mut Self {
         unsafe { self.native_mut().setTextSkewX(skew_x) }
+        self
     }
 
     // TODO: getTextBlobIntercepts
@@ -434,4 +464,14 @@ impl Handle<SkPaint> {
     pub fn nothing_to_draw(&self) -> bool {
         unsafe { self.native().nothingToDraw() }
     }
+}
+
+#[test]
+fn default_creation() {
+    let mut paint = Paint::default();
+}
+
+fn method_chaining_compiles() {
+    let mut paint = Paint::default();
+    let paint = paint.reset().reset();
 }
