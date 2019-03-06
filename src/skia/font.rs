c0308360974b548b70262287eb0d277c8c5b2502
Armin Sander <armin@replicator.org>
2019-03-06 19:25:25 +0100

    review font, utilize transmutation

--- src/skia/font.rs
+++ src/skia/font.rs
@@ -23,7 +23,8 @@ use crate::skia::{
     Typeface,
     FontHinting,
     Paint,
-    FontMetrics
+    FontMetrics,
+    scalar
 };
 
 pub type FontEdging = EnumHandle<SkFont_Edging>;
@@ -59,33 +60,36 @@ impl Handle<SkFont> {
 
     pub fn from_typeface(typeface: &Typeface) -> Self {
         let mut font : SkFont = unsafe { mem::uninitialized() };
-        unsafe { C_SkFont_ConstructFromTypeface(&mut font, typeface.shared_native()) }
+        unsafe {
+            C_SkFont_ConstructFromTypeface(&mut font, typeface.shared_native())
+        }
         font.into_handle()
     }
 
-    pub fn from_typeface_with_size(typeface: &Typeface, size: f32) -> Self {
+    pub fn from_typeface_with_size(typeface: &Typeface, size: scalar) -> Self {
         let mut font : SkFont = unsafe { mem::uninitialized() };
-        unsafe { C_SkFont_ConstructFromTypefaceWithSize(&mut font, typeface.shared_native(), size) }
+        unsafe {
+            C_SkFont_ConstructFromTypefaceWithSize(&mut font, typeface.shared_native(), size)
+        }
         font.into_handle()
     }
 
-    pub fn from_typeface_with_size_scale_and_skew(typeface: &Typeface, size: f32, scale: f32, skew: f32) -> Self {
+    pub fn from_typeface_with_size_scale_and_skew(typeface: &Typeface, size: scalar, scale: scalar, skew: scalar) -> Self {
         let mut font : SkFont = unsafe { mem::uninitialized() };
-        unsafe { C_SkFont_ConstructFromTypefaceWithSizeScaleAndSkew(&mut font, typeface.shared_native(), size, scale, skew) }
+        unsafe {
+            C_SkFont_ConstructFromTypefaceWithSizeScaleAndSkew(&mut font, typeface.shared_native(), size, scale, skew)
+        }
         font.into_handle()
     }
 
-    // suggestion: forces_auto_hinting().
     pub fn is_force_auto_hinting(&self) -> bool {
         unsafe { self.native().isForceAutoHinting() }
     }
 
-    // suggestion: has_embedded_bitmaps().
     pub fn is_embedded_bitmaps(&self) -> bool {
         unsafe { self.native().isEmbeddedBitmaps() }
     }
 
-    // supports_subpixel?
     pub fn is_subpixel(&self) -> bool {
         unsafe { self.native().isSubpixel() }
     }
@@ -99,44 +103,69 @@ impl Handle<SkFont> {
         unsafe { self.native().isEmbolden() }
     }
 
-    pub fn set_force_autohinting(&mut self, force_auto_hinting: bool) {
-        unsafe { self.native_mut().setForceAutoHinting(force_auto_hinting) }
+    pub fn set_force_autohinting(&mut self, force_auto_hinting: bool) -> &mut Self {
+        unsafe {
+            self.native_mut().setForceAutoHinting(force_auto_hinting)
+        }
+        self
     }
 
-    pub fn set_embedded_bitmaps(&mut self, embedded_bitmaps: bool) {
-        unsafe { self.native_mut().setEmbeddedBitmaps(embedded_bitmaps) }
+    pub fn set_embedded_bitmaps(&mut self, embedded_bitmaps: bool) -> &mut Self {
+        unsafe {
+            self.native_mut().setEmbeddedBitmaps(embedded_bitmaps)
+        }
+        self
     }
 
-    pub fn set_subpixel(&mut self, subpixel: bool) {
-        unsafe { self.native_mut().setSubpixel(subpixel) }
+    pub fn set_subpixel(&mut self, subpixel: bool) -> &mut Self {
+        unsafe {
+            self.native_mut().setSubpixel(subpixel)
+        }
+        self
     }
 
-    pub fn set_linear_metrics(&mut self, linear_metrics: bool) {
-        unsafe { self.native_mut().setLinearMetrics(linear_metrics) }
+    pub fn set_linear_metrics(&mut self, linear_metrics: bool) -> &mut Self {
+        unsafe {
+            self.native_mut().setLinearMetrics(linear_metrics)
+        }
+        self
     }
 
-    pub fn set_embolden(&mut self, embolden: bool) {
-        unsafe { self.native_mut().setEmbolden(embolden) }
+    pub fn set_embolden(&mut self, embolden: bool) -> &mut Self {
+        unsafe {
+            self.native_mut().setEmbolden(embolden)
+        }
+        self
     }
 
     pub fn edging(&self) -> FontEdging {
-        FontEdging::from_native(unsafe { self.native().getEdging() })
+        FontEdging::from_native(unsafe {
+            self.native().getEdging()
+        })
     }
 
-    pub fn set_edging(&mut self, edging: FontEdging) {
-        unsafe { self.native_mut().setEdging(edging.into_native()) }
+    pub fn set_edging(&mut self, edging: FontEdging) -> &mut Self {
+        unsafe {
+            self.native_mut().setEdging(edging.into_native())
+        }
+        self
     }
 
-    pub fn set_hinting(&mut self, hinting: FontHinting) {
-        unsafe { self.native_mut().setHinting(hinting.into_native()) }
+    pub fn set_hinting(&mut self, hinting: FontHinting) -> &mut Self {
+        unsafe {
+            self.native_mut().setHinting(hinting.into_native())
+        }
+        self
     }
 
     pub fn hinting(&self) -> FontHinting {
-        FontHinting::from_native(unsafe { self.native().getHinting() })
+        FontHinting::from_native(unsafe {
+            self.native().getHinting()
+        })
     }
 
     #[warn(unused)]
-    pub fn with_size(&self, size: f32) -> Option<Self> {
+    pub fn with_size(&self, size: scalar) -> Option<Self> {
         if size >= 0.0 && !size.is_infinite() && !size.is_nan() {
             let mut font = unsafe { SkFont::new() };
             unsafe { C_SkFont_makeWithSize(self.native(), size, &mut font) }
@@ -147,38 +176,47 @@ impl Handle<SkFont> {
     }
 
     pub fn typeface(&self) -> Typeface {
-        Typeface::from_unshared_ptr(unsafe { self.native().getTypeface() }).unwrap()
+        Typeface::from_unshared_ptr(unsafe {
+            self.native().getTypeface()
+        }).unwrap()
     }
 
-    pub fn size(&self) -> f32 {
+    pub fn size(&self) -> scalar {
         unsafe { self.native().getSize() }
     }
 
-    pub fn scale_x(&self) -> f32 {
+    pub fn scale_x(&self) -> scalar {
         unsafe { self.native().getScaleX() }
     }
 
-    pub fn skew_y(&self) -> f32 {
+    pub fn skew_y(&self) -> scalar {
         unsafe { self.native().getSkewX() }
     }
 
-    pub fn set_typeface(&mut self, tf: &Typeface) {
-        unsafe { C_SkFont_setTypeface(self.native_mut(), tf.shared_native()) }
+    pub fn set_typeface(&mut self, tf: &Typeface) -> &mut Self {
+        unsafe {
+            C_SkFont_setTypeface(self.native_mut(), tf.shared_native())
+        }
+        self
     }
 
-    pub fn set_size(&mut self, size: f32) {
+    pub fn set_size(&mut self, size: scalar) -> &mut Self {
         unsafe { self.native_mut().setSize(size) }
+        self
     }
 
-    pub fn set_scale_x(&mut self, scale_x: f32) {
+    pub fn set_scale_x(&mut self, scale_x: scalar) -> &mut Self {
         unsafe { self.native_mut().setScaleX(scale_x) }
+        self
     }
 
-    pub fn set_skew_x(&mut self, skew_x: f32) {
+    pub fn set_skew_x(&mut self, skew_x: scalar) -> &mut Self {
         unsafe { self.native_mut().setSkewX(skew_x) }
+        self
     }
 
-    // we support only UTF8 for now.
+    // we support UTF8 for now only.
+    // TODO: can we return a slice _into_ glyphs?
     pub fn str_to_glyphs(&self, str: &str, glyphs: &mut[GlyphId]) -> usize {
         let bytes = str.as_bytes();
 
@@ -187,7 +225,7 @@ impl Handle<SkFont> {
             bytes.len(),
             TextEncoding::UTF8.into_native(),
             glyphs.as_mut_ptr(),
-            glyphs.len().max(i32::max_value().try_into().unwrap()).try_into().unwrap())
+            glyphs.len().min(i32::max_value().try_into().unwrap()).try_into().unwrap())
             .try_into().unwrap()
         }
     }
@@ -223,10 +261,10 @@ impl Handle<SkFont> {
     }
 
     // note that the returned usize value is the bytes of the str that fits and not the characters.
-    pub fn break_str(&self, str: &str, max_width: f32) -> (usize, f32) {
+    pub fn break_str(&self, str: &str, max_width: scalar) -> (usize, scalar) {
         let bytes = str.as_bytes();
 
-        let mut measured_width = 0.0;
+        let mut measured_width = scalar::default();
         let bytes_fit = unsafe { self.native()
             .breakText(
                 bytes.as_ptr() as _, bytes.len(), TextEncoding::UTF8.into_native(),
@@ -235,49 +273,35 @@ impl Handle<SkFont> {
         (bytes_fit, measured_width)
     }
 
-    pub fn measure_str(&self, str: &str, paint: Option<&Paint>) -> (f32, Rect) {
+    pub fn measure_str(&self, str: &str, paint: Option<&Paint>) -> (scalar, Rect) {
         let bytes = str.as_bytes();
-        let mut bounds = SkRect {
-            fLeft: 0.0,
-            fTop: 0.0,
-            fRight: 0.0,
-            fBottom: 0.0
-        };
+        let mut bounds = Rect::default();
 
         let width = unsafe { self.native()
             .measureText1(
                 bytes.as_ptr() as _, bytes.len(), TextEncoding::UTF8.into_native(),
-                &mut bounds, paint.native_ptr_or_null()) };
+                bounds.native_mut(), paint.native_ptr_or_null()) };
 
-        (width, Rect::from_native(bounds))
+        (width, bounds)
     }
 
     pub fn widths_bounds(
         &self,
         glyphs: &[u16],
-        widths: Option<&mut [f32]>,
+        widths: Option<&mut [scalar]>,
         bounds: Option<&mut [Rect]>,
         paint: Option<&Paint>) {
         let count = glyphs.len();
-        let mut widths = widths;
+
         let mut bounds = bounds;
+        let mut widths = widths;
 
-        if let Some(slice) = &widths { assert_eq!(count, slice.len()) };
-        if let Some(slice) = &bounds { assert_eq!(count, slice.len()) };
+        {
+            if let Some(slice) = &widths { assert_eq!(count, slice.len()) };
+            if let Some(slice) = &bounds { assert_eq!(count, slice.len()) };
+        }
 
-        let mut bounds_vec : Option<Vec<SkRect>> = match &bounds {
-            Some(_) => {
-                Some(vec![SkRect {
-                    fLeft: 0.0,
-                    fTop: 0.0,
-                    fRight: 0.0,
-                    fBottom: 0.0
-                }; count])
-            },
-            None => None
-        };
-
-        let bounds_ptr = bounds_vec.as_ptr_or_null_mut();
+        let bounds_ptr = bounds.native_mut().as_ptr_or_null_mut();
         let widths_ptr = widths.as_ptr_or_null_mut();
         let paint_ptr = paint.native_ptr_or_null();
 
@@ -287,61 +311,53 @@ impl Handle<SkFont> {
                 count.try_into().unwrap(),
                 widths_ptr, bounds_ptr, paint_ptr)
         }
-
-        if let (Some(vec), Some(bounds)) = (&bounds_vec, &mut bounds) {
-            vec.iter().enumerate().for_each(|(i, r)| bounds[i] = Rect::from_native(*r))
-        }
     }
 
     pub fn pos(&self, glyphs: &[u16], pos: &mut [Point], origin: Option<Point>) {
         let count = glyphs.len();
         assert_eq!(count, pos.len());
 
-        let mut pos_vec : Vec<SkPoint> =
-            vec![SkPoint {
-                fX: 0.0,
-                fY: 0.0
-            }; count];
-
-        let origin = origin.map(|p| p.into_native()).unwrap_or(SkPoint { fX: 0.0, fY: 0.0 });
+        let origin = origin.unwrap_or(Point::default());
 
         unsafe {
             self.native().getPos(
                 glyphs.as_ptr(),
                 count.try_into().unwrap(),
-                pos_vec.as_mut_ptr(),
-                origin)
+                pos.native_mut().as_mut_ptr(),
+                origin.native().clone())
         }
-
-        pos_vec
-            .iter()
-            .enumerate()
-            .for_each(|(i, p)| pos[i] = Point::from_native(*p))
     }
 
-    pub fn x_pos(&self, glyphs: &[u16], xpos: &mut [f32], origin: Option<f32>) {
+    pub fn x_pos(&self, glyphs: &[u16], xpos: &mut [scalar], origin: Option<scalar>) {
         let count = glyphs.len();
         assert_eq!(count, xpos.len());
         let origin = origin.unwrap_or_default();
 
         unsafe {
-            self.native().getXPos(glyphs.as_ptr(), count.try_into().unwrap(), xpos.as_mut_ptr(), origin)
+            self.native().getXPos(
+                glyphs.as_ptr(),
+                count.try_into().unwrap(),
+                xpos.as_mut_ptr(),
+                origin)
         }
     }
 
     pub fn path(&self, glyph_id: u16) -> Option<Path> {
         let mut path = Path::default();
-        unsafe { self.native().getPath(glyph_id, path.native_mut())}
-            .if_true_some(path)
+        unsafe {
+            self.native().getPath(glyph_id, path.native_mut())
+        }.if_true_some(path)
     }
 
-    pub fn metrics(&self) -> (f32, FontMetrics) {
+    pub fn metrics(&self) -> (scalar, FontMetrics) {
         let mut fm = unsafe { mem::zeroed() };
         let line_spacing = unsafe { self.native().getMetrics(&mut fm) };
         (line_spacing, FontMetrics::from_native(fm))
     }
 
-    pub fn spacing(&self) -> f32 {
-        unsafe { self.native().getMetrics(ptr::null_mut()) }
+    pub fn spacing(&self) -> scalar {
+        unsafe {
+            self.native().getMetrics(ptr::null_mut())
+        }
     }
 }
