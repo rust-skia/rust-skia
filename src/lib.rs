43107a2bfe03776e53fb9f7e825184c9b521d05f
Armin Sander <armin@replicator.org>
2019-03-06 19:59:58 +0100

    add a parnoia test

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,22 @@ extern crate bitflags;
 #[macro_use]
 extern crate lazy_static;
 
+
+mod transmutation_tests {
+
+    use crate::prelude::NativeTransmutableSliceAccess;
+    use crate::skia::Point;
+    use rust_skia::SkPoint;
+
+    #[test]
+    fn test_transmutation_of_fixed_size_arrays_to_slice() {
+        let mut points = [Point::default(); 4];
+
+        let points_native = points.native_mut();
+        let native_point = SkPoint { fX: 10.0, fY: 11.0 };
+        points_native[1] = native_point;
+
+        assert_eq!(points[1].x, native_point.fX);
+        assert_eq!(points[1].y, native_point.fY);
+    }
+}
