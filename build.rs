f70ab5033f9e867792cf60f15e9df92448dfc568
Armin Sander <armin@replicator.org>
2019-03-06 15:03:21 +0100

    another linker error appeared spontaneously, fixed by adding a fontsub dependency

--- build.rs
+++ build.rs
@@ -132,6 +132,7 @@ fn main() {
     println!("cargo:rustc-link-lib=ole32");
     println!("cargo:rustc-link-lib=user32");
     println!("cargo:rustc-link-lib=gdi32");
+    println!("cargo:rustc-link-lib=fontsub");
 
     // required since GrContext::MakeVulkan is linked.
     if cfg!(feature="vulkan") {
