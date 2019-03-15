9112019c86b0cbcaf75f007f5c3d16696bbb10f1
Armin Sander <armin@replicator.org>
2019-03-15 17:26:18 +0100

    the provided table's are optional in the TableColorFilter

--- skia-safe/src/effects/table_color_filter.rs
+++ skia-safe/src/effects/table_color_filter.rs
@@ -12,9 +12,13 @@ impl TableColorFilter {
         }).unwrap()
     }
 
-    pub fn from_argb(table_a: &[u8; 256], table_r: &[u8; 256], table_g: &[u8; 256], table_b: &[u8; 256] ) -> ColorFilter {
+    pub fn from_argb(table_a: Option<&[u8; 256]>, table_r: Option<&[u8; 256]>, table_g: Option<&[u8; 256]>, table_b: Option<&[u8; 256]> ) -> ColorFilter {
         ColorFilter::from_ptr(unsafe {
-            C_SkTableColorFilter_MakeARGB(table_a.as_ptr(), table_r.as_ptr(), table_g.as_ptr(), table_b.as_ptr())
+            C_SkTableColorFilter_MakeARGB(
+                table_a.as_ptr_or_null() as _,
+                table_r.as_ptr_or_null() as _,
+                table_g.as_ptr_or_null() as _,
+                table_b.as_ptr_or_null() as _)
         }).unwrap()
     }
 }
