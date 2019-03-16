b26f8bc1881931c8c1a42d918da3b4c129a6e8a6
Armin Sander <armin@replicator.org>
2019-03-16 13:38:53 +0100

    remove unused casts

--- skia-safe/src/effects/table_color_filter.rs
+++ skia-safe/src/effects/table_color_filter.rs
@@ -15,10 +15,10 @@ impl TableColorFilter {
     pub fn from_argb(table_a: Option<&[u8; 256]>, table_r: Option<&[u8; 256]>, table_g: Option<&[u8; 256]>, table_b: Option<&[u8; 256]> ) -> ColorFilter {
         ColorFilter::from_ptr(unsafe {
             C_SkTableColorFilter_MakeARGB(
-                table_a.map(|t| t.as_ref()).as_ptr_or_null() as _,
-                table_r.map(|t| t.as_ref()).as_ptr_or_null() as _,
-                table_g.map(|t| t.as_ref()).as_ptr_or_null() as _,
-                table_b.map(|t| t.as_ref()).as_ptr_or_null() as _)
+                table_a.map(|t| t.as_ref()).as_ptr_or_null(),
+                table_r.map(|t| t.as_ref()).as_ptr_or_null(),
+                table_g.map(|t| t.as_ref()).as_ptr_or_null(),
+                table_b.map(|t| t.as_ref()).as_ptr_or_null())
         }).unwrap()
     }
 }
