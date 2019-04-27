dc906c53771fd33fafee9ea65069803595553754
Armin Sander <armin@replicator.org>
2019-04-27 14:43:41 +0200

    arithmetic_image_filter clippy and rustfmt

--- skia-safe/src/effects/arithmetic_image_filter.rs
+++ skia-safe/src/effects/arithmetic_image_filter.rs
@@ -1,19 +1,33 @@
 use crate::prelude::*;
-
-use skia_bindings::C_SkArithmeticImageFilter_Make;
 use crate::{ImageFilter, ImageFilterCropRect};
+use skia_bindings::C_SkArithmeticImageFilter_Make;
 
 pub enum ArithmeticImageFilter {}
 
 impl ArithmeticImageFilter {
-
-    pub fn new<'a, CR: Into<Option<&'a ImageFilterCropRect>>>(k1: f32, k2: f32, k3: f32, k4: f32, enforce_pm_color: bool, background: &ImageFilter, foreground: &ImageFilter, crop_rect: CR) -> Option<ImageFilter> {
+    #[allow(clippy::new_ret_no_self)]
+    #[allow(clippy::too_many_arguments)]
+    pub fn new<'a, CR: Into<Option<&'a ImageFilterCropRect>>>(
+        k1: f32,
+        k2: f32,
+        k3: f32,
+        k4: f32,
+        enforce_pm_color: bool,
+        background: &ImageFilter,
+        foreground: &ImageFilter,
+        crop_rect: CR,
+    ) -> Option<ImageFilter> {
         ImageFilter::from_ptr(unsafe {
-            C_SkArithmeticImageFilter_Make(k1, k2, k3, k4, enforce_pm_color, background.shared_native(), foreground.shared_native(), crop_rect.into().native_ptr_or_null())
+            C_SkArithmeticImageFilter_Make(
+                k1,
+                k2,
+                k3,
+                k4,
+                enforce_pm_color,
+                background.shared_native(),
+                foreground.shared_native(),
+                crop_rect.into().native_ptr_or_null(),
+            )
         })
-
-
-
-
     }
 }
