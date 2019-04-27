d6483dad3f39bc89c72a701d3bfd806e47b02f3b
Armin Sander <armin@replicator.org>
2019-04-27 17:15:28 +0200

    FilterQuality rustfmt

--- skia-safe/src/core/filter_quality.rs
+++ skia-safe/src/core/filter_quality.rs
@@ -7,8 +7,11 @@ pub enum FilterQuality {
     None = SkFilterQuality::kNone_SkFilterQuality as _,
     Low = SkFilterQuality::kLow_SkFilterQuality as _,
     Medium = SkFilterQuality::kMedium_SkFilterQuality as _,
-    High = SkFilterQuality::kHigh_SkFilterQuality as _
+    High = SkFilterQuality::kHigh_SkFilterQuality as _,
 }
 
 impl NativeTransmutable<SkFilterQuality> for FilterQuality {}
-#[test] fn test_filter_quality_layout() { FilterQuality::test_layout() }
+#[test]
+fn test_filter_quality_layout() {
+    FilterQuality::test_layout()
+}
