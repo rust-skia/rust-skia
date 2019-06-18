85b6ae86dab4e139a5e7e4fcc4adf64da4563bfd
Armin Sander <armin@replicator.org>
2019-06-18 15:20:33 +0200

    m76: update core/color_filter.rs

--- skia-safe/src/core/color_filter.rs
+++ skia-safe/src/core/color_filter.rs
@@ -1,7 +1,7 @@
 use crate::prelude::*;
 use crate::{scalar, BlendMode, Color, Color4f, ColorSpace, NativeFlattenable};
 use skia_bindings::{
-    C_SkColorFilter_Deserialize, C_SkColorFilter_asColorMatrix, C_SkColorFilter_asColorMode,
+    C_SkColorFilter_Deserialize, C_SkColorFilter_asAColorMatrix, C_SkColorFilter_asAColorMode,
     C_SkColorFilter_getFlags, C_SkColorFilter_makeComposed, SkColorFilter,
     SkColorFilter_Flags_kAlphaUnchanged_Flag, SkFlattenable, SkRefCntBase,
 };
@@ -34,19 +34,31 @@ impl NativeFlattenable for SkColorFilter {
 impl RCHandle<SkColorFilter> {
     #[deprecated(since = "0.12.0", note = "use to_color_mode()")]
     pub fn as_color_mode(&self) -> Option<(Color, BlendMode)> {
-        self.to_color_mode()
+        self.to_a_color_mode()
     }
 
+    #[deprecated(note = "use to_a_color_mode()")]
     pub fn to_color_mode(&self) -> Option<(Color, BlendMode)> {
+        self.to_a_color_mode()
+    }
+
+    pub fn to_a_color_mode(&self) -> Option<(Color, BlendMode)> {
         let mut color: Color = 0.into();
         let mut mode: BlendMode = BlendMode::default();
-        unsafe { C_SkColorFilter_asColorMode(self.native(), color.native_mut(), mode.native_mut()) }
-            .if_true_some((color, mode))
+        unsafe {
+            C_SkColorFilter_asAColorMode(self.native(), color.native_mut(), mode.native_mut())
+        }
+        .if_true_some((color, mode))
     }
 
+    #[deprecated(note = "use to_a_color_matrix()")]
     pub fn to_color_matrix(&self) -> Option<[scalar; 20]> {
+        self.to_a_color_matrix()
+    }
+
+    pub fn to_a_color_matrix(&self) -> Option<[scalar; 20]> {
         let mut matrix: [scalar; 20] = Default::default();
-        unsafe { C_SkColorFilter_asColorMatrix(self.native(), matrix.as_mut_ptr()) }
+        unsafe { C_SkColorFilter_asAColorMatrix(self.native(), matrix.as_mut_ptr()) }
             .if_true_some(matrix)
     }
 
@@ -78,11 +90,11 @@ impl RCHandle<SkColorFilter> {
 
 pub mod color_filters {
     use crate::prelude::*;
-    use crate::{scalar, BlendMode, Color, ColorFilter};
+    use crate::{scalar, BlendMode, Color, ColorFilter, ColorMatrix};
     use skia_bindings::{
         C_SkColorFilters_Blend, C_SkColorFilters_Compose, C_SkColorFilters_Lerp,
-        C_SkColorFilters_LinearToSRGBGamma, C_SkColorFilters_MatrixRowMajor255,
-        C_SkColorFilters_SRGBToLinearGamma,
+        C_SkColorFilters_LinearToSRGBGamma, C_SkColorFilters_Matrix,
+        C_SkColorFilters_MatrixRowMajor, C_SkColorFilters_SRGBToLinearGamma,
     };
 
     pub fn compose(outer: &ColorFilter, inner: &ColorFilter) -> Option<ColorFilter> {
@@ -91,9 +103,17 @@ pub mod color_filters {
         })
     }
 
+    pub fn matrix(color_matrix: &ColorMatrix) -> ColorFilter {
+        ColorFilter::from_ptr(unsafe { C_SkColorFilters_Matrix(color_matrix.native()) }).unwrap()
+    }
+
+    #[deprecated(note = "use matrix_row_major()")]
     pub fn matrix_row_major_255(array: &[scalar; 20]) -> ColorFilter {
-        ColorFilter::from_ptr(unsafe { C_SkColorFilters_MatrixRowMajor255(array.as_ptr()) })
-            .unwrap()
+        self::matrix_row_major(array)
+    }
+
+    pub fn matrix_row_major(array: &[scalar; 20]) -> ColorFilter {
+        ColorFilter::from_ptr(unsafe { C_SkColorFilters_MatrixRowMajor(array.as_ptr()) }).unwrap()
     }
 
     pub fn blend(c: impl Into<Color>, mode: BlendMode) -> Option<ColorFilter> {
