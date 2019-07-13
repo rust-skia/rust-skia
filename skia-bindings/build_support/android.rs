710bc0542e36626cb376177accf9041446d7f83c
Armin Sander <armin@replicator.org>
2019-07-13 11:02:41 +0200

    support cross platform builds for x86_64-linux-android on macOS and Linux

--- skia-bindings/build_support/android.rs
+++ skia-bindings/build_support/android.rs
@@ -1,5 +1,26 @@
 use std::env;
 
+pub fn additional_clang_args(target: &str, arch: &str) -> Vec<String> {
+    let mut args: Vec<String> = Vec::new();
+
+    match arch {
+        "i686" => args.push("-m32".into()),
+        "x86_64" => args.push("-m64".into()),
+        _ => {}
+    }
+
+    let ndk = ndk();
+
+    args.push(format!("--sysroot={}/sysroot", ndk));
+    args.push(format!("-I{}/sysroot/usr/include/{}", ndk, target));
+    args.push(format!(
+        "-isystem{}/sources/cxx-stl/llvm-libc++/include",
+        ndk
+    ));
+    args.push(format!("--target={}", target));
+    args
+}
+
 pub fn ndk() -> String {
     env::var("ANDROID_NDK").expect("ANDROID_NDK variable not set")
 }
--- skia-bindings/build_support/clang.rs
+++ skia-bindings/build_support/clang.rs
@@ -1,8 +1,8 @@
 /// Convert a Rust target architecture identifier to a clang target architecture identifier.
 pub fn target_arch(arch: &str) -> &str {
-    if arch == "aarch64" {
-        "arm64"
-    } else {
-        arch
+    match arch {
+        "aarch64" => "arm64",
+        "i686" => "x86",
+        arch => arch,
     }
 }
