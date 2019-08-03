07e87b8df0f420f58cc39aeeb130ded3b91e9163
Armin Sander <armin@replicator.org>
2019-08-03 10:38:37 +0200

    add skshaper example to the skia-org command line application and update readme

---
+++ skia-safe/examples/skia-org/skshaper_example.rs
@@ -0,0 +1,43 @@
+use crate::artifact::DrawingDriver;
+use skia_safe::{icu, Canvas, Font, Paint, Point, Shaper, Typeface};
+use std::path::PathBuf;
+
+pub fn draw<Driver: DrawingDriver>(path: &PathBuf) {
+    let path = path.join("SkShaper-Example");
+
+    icu::init();
+
+    Driver::draw_image_256(&path, "rtl-shaped", draw_rtl_shaped);
+    Driver::draw_image_256(&path, "rtl-unshaped", draw_rtl_unshaped);
+}
+
+const RTL_TEXT: &str = "العربية";
+const TEXT_POS: Point = Point::new(0.0, 64.0);
+
+fn draw_rtl_shaped(canvas: &mut Canvas) {
+    let mut paint = Paint::default();
+    paint.set_anti_alias(true);
+
+    let font = &Font::from_typeface(&Typeface::default(), 64.0);
+
+    let shaper = Shaper::new();
+    if let Some((blob, _)) =
+        shaper.shape_text_blob(RTL_TEXT, font, false, 10000.0, Point::default())
+    {
+        canvas.draw_text_blob(&blob, TEXT_POS, &paint)
+    }
+}
+
+fn draw_rtl_unshaped(canvas: &mut Canvas) {
+    let mut paint = Paint::default();
+    paint.set_anti_alias(true);
+
+    let font = &Font::from_typeface(&Typeface::default(), 64.0);
+
+    let shaper = Shaper::new_primitive();
+    if let Some((blob, _)) =
+        shaper.shape_text_blob(RTL_TEXT, font, false, 10000.0, Point::default())
+    {
+        canvas.draw_text_blob(&blob, TEXT_POS, &paint)
+    }
+}
