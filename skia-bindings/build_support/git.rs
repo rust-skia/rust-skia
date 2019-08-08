b138ae839419d02af5329e42cf732b0c51946369
Armin Sander <armin@replicator.org>
2019-08-08 23:05:56 +0200

    prevent two warnings

--- skia-bindings/build_support/git.rs
+++ skia-bindings/build_support/git.rs
@@ -25,9 +25,9 @@ pub fn trim_hash(hash: &str) -> String {
 /// Run git with the given args in the given directory, print stderr to the current
 /// process's terminal, and capture its stdout output.
 /// Panics if the git command fails.
-pub fn run<'a>(args: &[impl AsRef<str>], dir: impl Into<Option<&'a Path>>) -> Vec<u8> {
+pub fn _run<'a>(args: &[impl AsRef<str>], dir: impl Into<Option<&'a Path>>) -> Vec<u8> {
     let args: Vec<&str> = args.iter().map(|s| s.as_ref()).collect();
-    let (status, output) = run2(&args, dir);
+    let (status, output) = _run2(&args, dir);
     if status == 0 {
         return output;
     }
@@ -37,7 +37,7 @@ pub fn run<'a>(args: &[impl AsRef<str>], dir: impl Into<Option<&'a Path>>) -> Ve
 /// Like run, but returns the status code _and_ the output or None if
 /// there is no status code (for example the command was interrupted).
 /// Panics if the git command could not be run at all.
-pub fn run2<'a>(args: &[impl AsRef<str>], dir: impl Into<Option<&'a Path>>) -> (i32, Vec<u8>) {
+pub fn _run2<'a>(args: &[impl AsRef<str>], dir: impl Into<Option<&'a Path>>) -> (i32, Vec<u8>) {
     let args: Vec<&str> = args.iter().map(|s| s.as_ref()).collect();
 
     let mut cmd = Command::new("git");
