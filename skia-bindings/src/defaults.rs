2da94f44e03c26965706fb95de06ec534e5b015c
Armin Sander <armin@replicator.org>
2019-08-20 18:05:34 +0200

    compile defaults for textlayout handles only when the feature is enabled

--- skia-bindings/src/defaults.rs
+++ skia-bindings/src/defaults.rs
@@ -1,53 +1,56 @@
 //! This file contains Default trait implementations for types that are
 //! used without a handle in skia-safe and get reexported from there.
 
-impl Default for crate::skia_textlayout_Affinity {
-    fn default() -> Self {
-        Self::Upstream
-    }
-}
-
-impl Default for crate::skia_textlayout_RectHeightStyle {
-    fn default() -> Self {
-        Self::Tight
-    }
-}
-
-impl Default for crate::skia_textlayout_RectWidthStyle {
-    fn default() -> Self {
-        Self::Tight
-    }
-}
-
-impl Default for crate::skia_textlayout_TextAlign {
-    fn default() -> Self {
-        Self::Left
-    }
-}
-
-impl Default for crate::skia_textlayout_PositionWithAffinity {
-    fn default() -> Self {
-        Self {
-            position: 0,
-            affinity: Default::default(),
+#[cfg(feature = "textlayout")]
+pub mod textlayout {
+    impl Default for crate::skia_textlayout_Affinity {
+        fn default() -> Self {
+            Self::Upstream
         }
     }
-}
 
-impl Default for crate::skia_textlayout_TextBaseline {
-    fn default() -> Self {
-        Self::Alphabetic
+    impl Default for crate::skia_textlayout_RectHeightStyle {
+        fn default() -> Self {
+            Self::Tight
+        }
     }
-}
 
-impl Default for crate::skia_textlayout_TextDecorationStyle {
-    fn default() -> Self {
-        Self::Solid
+    impl Default for crate::skia_textlayout_RectWidthStyle {
+        fn default() -> Self {
+            Self::Tight
+        }
     }
-}
 
-impl Default for crate::skia_textlayout_StyleType {
-    fn default() -> Self {
-        Self::AllAttributes
+    impl Default for crate::skia_textlayout_TextAlign {
+        fn default() -> Self {
+            Self::Left
+        }
+    }
+
+    impl Default for crate::skia_textlayout_PositionWithAffinity {
+        fn default() -> Self {
+            Self {
+                position: 0,
+                affinity: Default::default(),
+            }
+        }
+    }
+
+    impl Default for crate::skia_textlayout_TextBaseline {
+        fn default() -> Self {
+            Self::Alphabetic
+        }
+    }
+
+    impl Default for crate::skia_textlayout_TextDecorationStyle {
+        fn default() -> Self {
+            Self::Solid
+        }
+    }
+
+    impl Default for crate::skia_textlayout_StyleType {
+        fn default() -> Self {
+            Self::AllAttributes
+        }
     }
 }
