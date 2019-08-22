0aa5c023c4d4cd3ab482c69e32bd90cb4995df4b
Armin Sander <armin@replicator.org>
2019-08-22 16:57:03 +0200

    suppress the improper-ctypes warning in bindings.rs on macOS

--- skia-bindings/build_support/skia.rs
+++ skia-bindings/build_support/skia.rs
@@ -438,6 +438,8 @@ fn bindgen_gen(build: &FinalBuildConfiguration, current_dir: &Path, output_direc
             non_exhaustive: false,
         })
         .raw_line("#![allow(clippy::all)]")
+        // GrVkBackendContext contains u128 fields on macOS
+        .raw_line("#![allow(improper_ctypes)]")
         .constified_enum(".*Mask")
         .constified_enum(".*Flags")
         .constified_enum(".*Bits")
