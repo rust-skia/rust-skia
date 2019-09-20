fedcc4c39b60b9e8c4dd778284738dec4427982a
Armin Sander <armin@replicator.org>
2019-09-20 09:46:39 +0200

    wrap effects/SkLumaColorFilter.h

---
+++ skia-safe/src/effects/luma_color_filter.rs
@@ -0,0 +1,16 @@
+use crate::prelude::*;
+use crate::ColorFilter;
+use skia_bindings as sb;
+use skia_bindings::SkColorFilter;
+
+impl RCHandle<SkColorFilter> {
+    pub fn luma() -> ColorFilter {
+        new()
+    }
+}
+
+pub fn new() -> ColorFilter {
+    ColorFilter::from_ptr(unsafe { sb::C_SkLumaColorFilter_Make() }).unwrap()
+}
+
+// TODO: asFragmentProcessor()
