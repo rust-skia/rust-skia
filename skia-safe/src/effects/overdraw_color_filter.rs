cd2d30b1d349feed98a83974a1c6c3b3e367fd43
Armin Sander <armin@replicator.org>
2019-09-20 16:01:47 +0200

    effects review

--- skia-safe/src/effects/overdraw_color_filter.rs
+++ skia-safe/src/effects/overdraw_color_filter.rs
@@ -6,11 +6,11 @@ use skia_bindings::SkColorFilter;
 pub const NUM_COLORS: usize = 6;
 
 impl RCHandle<SkColorFilter> {
-    pub fn overdraw(colors: &[PMColor; 6]) -> ColorFilter {
+    pub fn overdraw(colors: &[PMColor; NUM_COLORS]) -> ColorFilter {
         new(colors)
     }
 }
 
-pub fn new(colors: &[PMColor; 6]) -> ColorFilter {
+pub fn new(colors: &[PMColor; NUM_COLORS]) -> ColorFilter {
     ColorFilter::from_ptr(unsafe { sb::C_SkOverdrawColorFilter_Make(colors.as_ptr()) }).unwrap()
 }
