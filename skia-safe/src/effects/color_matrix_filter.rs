b61f4549f8898a1b6ee1e456799bfc2f3e829150
Armin Sander <armin@replicator.org>
2019-09-20 09:40:43 +0200

    wrap effects/SkColorMatrixFilter.h

---
+++ skia-safe/src/effects/color_matrix_filter.rs
@@ -0,0 +1,19 @@
+use crate::prelude::*;
+use crate::{Color, ColorFilter, ImageFilter};
+use skia_bindings as sb;
+use skia_bindings::SkColorFilter;
+
+impl RCHandle<SkColorFilter> {
+    pub fn new_lighting(mul: impl Into<Color>, add: impl Into<Color>) -> Option<Self> {
+        new_lighting(mul, add)
+    }
+}
+
+pub fn new_lighting(mul: impl Into<Color>, add: impl Into<Color>) -> Option<ColorFilter> {
+    ColorFilter::from_ptr(unsafe {
+        sb::C_SkColorMatrixFilter_MakeLightingFilter(
+            mul.into().into_native(),
+            add.into().into_native(),
+        )
+    })
+}
