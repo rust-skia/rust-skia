5c53a265efea106802d5fe8d6007a83703562b40
Armin Sander <armin@replicator.org>
2020-01-15 20:50:47 +0100

    binding generation: don't suppress warnings related to mem::uninitialized()

--- skia-bindings/build_support/skia.rs
+++ skia-bindings/build_support/skia.rs
@@ -651,9 +651,6 @@ fn bindgen_gen(build: &FinalBuildConfiguration, current_dir: &Path, output_direc
         .raw_line("#![allow(clippy::all)]")
         // GrVkBackendContext contains u128 fields on macOS
         .raw_line("#![allow(improper_ctypes)]")
-        // mem::uninitialized()
-        .raw_line("#![allow(invalid_value)]")
-        .raw_line("#![allow(deprecated)]")
         .parse_callbacks(Box::new(ParseCallbacks))
         .whitelist_function("C_.*")
         .constified_enum(".*Mask")
