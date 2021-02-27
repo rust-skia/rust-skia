3d717b21904b9d9665b5f878e79355f3153595f4
superwhiskers <whiskerdev@protonmail.com>
2021-02-26 21:58:09 -0600

    rust-skia: add nix flake for a development shell

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "nix development shell for rust-skia";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages."${system}";
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ rustup python ];
+        };
+      });
+}
