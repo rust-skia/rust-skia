5c2a88f7937d03101c985ab743b2068351e49b3e
Armin Sander <armin@replicator.org>
2021-02-28 19:32:11 +0100

    m89: Fix formatting

--- skia-safe/src/gpu/direct_context.rs
+++ skia-safe/src/gpu/direct_context.rs
@@ -85,10 +85,7 @@ impl RCHandle<GrDirectContext> {
         options: impl Into<Option<&'a ContextOptions>>,
     ) -> Option<DirectContext> {
         DirectContext::from_ptr(unsafe {
-            sb::C_GrContext_MakeMetal(
-                backend.native(),
-                options.into().native_ptr_or_null(),
-            )
+            sb::C_GrContext_MakeMetal(backend.native(), options.into().native_ptr_or_null())
         })
     }
 
