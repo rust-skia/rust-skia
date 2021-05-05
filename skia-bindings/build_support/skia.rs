81d876667605508a457d476da0fc39cb69dcee57
Roman Petrenko <roman@canva.com>
2021-05-05 15:33:01 +1000

    Fix textlayout linking (#9)

--- skia-bindings/build_support/skia.rs
+++ skia-bindings/build_support/skia.rs
@@ -707,7 +707,7 @@ pub fn build_skia(
     ninja_command: &Path,
 ) {
     let ninja_status = Command::new(ninja_command)
-        .args(&[":skia", "-C", config.output_directory.to_str().unwrap()])
+        .args(&["-C", config.output_directory.to_str().unwrap()])
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
         .status();
