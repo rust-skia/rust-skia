037e871ce1e8281d5f7a3dc1c9238a3b57120307
Armin Sander <armin@replicator.org>
2021-07-22 09:05:03 +0200

    Clippy

--- skia-safe/src/interop/cpp.rs
+++ skia-safe/src/interop/cpp.rs
@@ -23,6 +23,7 @@ impl<T: 'static> VecSink<'_, T> {
         }
     }
 
+    #[allow(unused)]
     pub fn new_mut(v: &mut dyn FnMut(&mut [T])) -> VecSink<T> {
         VecSink {
             sink: sb::VecSink {
@@ -43,6 +44,7 @@ impl<T: 'static> VecSink<'_, T> {
         (rust_fn)(safer::from_raw_parts(ptr, len));
     }
 
+    #[allow(unused)]
     unsafe extern "C" fn set_fn_mut(ptr: *mut T, len: usize, rust_fn: TraitObject) {
         let rust_fn: &mut dyn FnMut(&mut [T]) = mem::transmute(rust_fn);
         (rust_fn)(safer::from_raw_parts_mut(ptr, len));
