9fc8584b886b5b5e94660b0fc12290f16e90543b
Armin Sander <armin@replicator.org>
2021-07-30 12:42:10 +0200

    Support ios-macabi targets

--- skia-bindings/build_support/ios.rs
+++ skia-bindings/build_support/ios.rs
@@ -4,18 +4,18 @@ use std::path::PathBuf;
 use std::process::{Command, Stdio};
 
 // TODO: add support for 32 bit devices and simulators.
-pub fn extra_skia_cflags(arch: &str, flags: &mut Vec<&str>) {
-    if is_simulator(arch) {
+pub fn extra_skia_cflags(arch: &str, abi: Option<&str>, flags: &mut Vec<&str>) {
+    if is_simulator(arch, abi) {
         flags.push("-mios-simulator-version-min=10.0");
     } else {
         flags.push("-miphoneos-version-min=10.0");
     }
 }
 
-pub fn additional_clang_args(arch: &str) -> Vec<String> {
+pub fn additional_clang_args(arch: &str, abi: Option<&str>) -> Vec<String> {
     let mut args: Vec<String> = Vec::new();
 
-    if is_simulator(arch) {
+    if is_simulator(arch, abi) {
         args.push("-mios-simulator-version-min=10.0".into());
         args.push("-m64".into());
     } else {
@@ -25,46 +25,52 @@ pub fn additional_clang_args(arch: &str) -> Vec<String> {
     }
 
     args.push("-isysroot".into());
-    args.push(sdk_path(arch).to_str().unwrap().into());
+    args.push(sdk_path(arch, abi).to_str().unwrap().into());
     args.push("-fembed-bitcode".into());
 
     args
 }
 
-/// Resolve the iOS SDK path by starting xcrun.
-fn sdk_path(arch: &str) -> PathBuf {
+/// Resolve the iOS SDK path by starting `xcrun`.
+fn sdk_path(arch: &str, abi: Option<&str>) -> PathBuf {
     let sdk_path = Command::new("xcrun")
         .arg("--show-sdk-path")
         .arg("--sdk")
-        .arg(if is_simulator(arch) {
-            "iphonesimulator"
-        } else {
-            "iphoneos"
-        })
+        .arg(sdk_name(arch, abi))
         .stderr(Stdio::inherit())
         .output()
-        .expect("failed to invoke xcrun")
+        .expect("Failed to invoke xcrun")
         .stdout;
 
     let string = String::from_utf8(sdk_path).expect("failed to resolve iOS SDK path");
     PathBuf::from(string.trim())
 }
 
-/// Returns true if the target architecture indicates that a simulator build is needed.
-fn is_simulator(arch: &str) -> bool {
-    matches!(arch, "x86_64")
+/// Returns `true` if the target architecture indicates that a simulator build is needed.
+fn is_simulator(arch: &str, abi: Option<&str>) -> bool {
+    sdk_name(arch, abi) == "iphonesimulator"
 }
 
-pub(crate) fn link_libraries(features: &Features) -> Vec<&str> {
+fn sdk_name(arch: &str, abi: Option<&str>) -> &'static str {
+    match () {
+        () if abi == Some("macabi") => "macosx",
+        () if arch == "x86_64" => "iphonesimulator",
+        () => "iphoneos",
+    }
+}
+
+pub(crate) fn link_libraries(abi: Option<&str>, features: &Features) -> Vec<&'static str> {
     let mut libs = vec![
         "c++",
-        "framework=MobileCoreServices",
         "framework=CoreFoundation",
         "framework=CoreGraphics",
         "framework=CoreText",
         "framework=ImageIO",
-        "framework=UIKit",
     ];
+
+    if abi != Some("macabi") {
+        libs.extend(["framework=MobileCoreServices", "framework=UIKit"]);
+    }
 
     if features.metal {
         libs.push("framework=Metal");
