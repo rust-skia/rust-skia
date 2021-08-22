bef9f911c01f7c9503815d6cb665c82d82f07113
Armin Sander <armin@replicator.org>
2021-08-22 21:41:03 +0200

    Guarantee that icu initialization is called one time only.

--- skia-bindings/src/icu.rs
+++ skia-bindings/src/icu.rs
@@ -1,28 +1,31 @@
 #[cfg(windows)]
 pub fn init() {
-    use std::env;
-    let icudtl = include_bytes!(concat!(env!("OUT_DIR"), "/skia/icudtl.dat"));
+    use std::{env, sync::Once};
+    ONCE.call_once(|| {
+        let icudtl = include_bytes!(concat!(env!("OUT_DIR"), "/skia/icudtl.dat"));
 
-    #[cfg(feature = "embed-icudtl")]
-    {
-        unsafe { crate::C_SetICU(&icudtl[0] as &'static u8 as *const u8 as _) };
-    }
+        #[cfg(feature = "embed-icudtl")]
+        {
+            unsafe { crate::C_SetICU(&icudtl[0] as &'static u8 as *const u8 as _) };
+        }
 
-    #[cfg(not(feature = "embed-icudtl"))]
-    {
-        use std::fs;
+        #[cfg(not(feature = "embed-icudtl"))]
+        {
+            use std::fs;
 
-        let path = env::current_exe()
-            .expect("failed to resolve the current executable's path")
-            .parent()
-            .expect("current executable's path does not point to a directory")
-            .join("icudtl.dat");
-        if path.exists() {
-            return;
-        };
-        fs::write(path, &icu_dtl[..])
-            .expect("failed to write icudtl.dat into the current executable's directory");
-    }
+            let path = env::current_exe()
+                .expect("failed to resolve the current executable's path")
+                .parent()
+                .expect("current executable's path does not point to a directory")
+                .join("icudtl.dat");
+            if path.exists() {
+                return;
+            };
+            fs::write(path, &icu_dtl[..])
+                .expect("failed to write icudtl.dat into the current executable's directory");
+        }
+    });
+    static ONCE: Once = Once::new();
 }
 
 #[cfg(not(windows))]
