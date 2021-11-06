f469899b00144f8e7ea799ab32a991d3e912baf9
Armin Sander <armin@replicator.org>
2021-11-06 19:03:59 +0100

    Make icu::init thread safe, closes #566

--- skia-bindings/src/icu.rs
+++ skia-bindings/src/icu.rs
@@ -5,7 +5,18 @@ pub fn init() {
 
     #[cfg(feature = "embed-icudtl")]
     {
+        use std::sync::Mutex;
+
+        lazy_static::lazy_static!(
+            static ref MUTEX : Mutex<()> = Mutex::new(());
+        );
+
+        // Using `Once` does not work for yet unknown reasons.
+        // https://github.com/rust-skia/rust-skia/issues/566
+
+        let lock = MUTEX.lock().unwrap();
         unsafe { crate::C_SetICU(&icudtl[0] as &'static u8 as *const u8 as _) };
+        drop(lock);
     }
 
     #[cfg(not(feature = "embed-icudtl"))]
