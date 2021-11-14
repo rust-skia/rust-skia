6a5846ad8a7a27b161a6b57d5455643be3119ebd
Armin Sander <armin@replicator.org>
2021-11-14 17:00:40 +0100

    m96: Add send / sync asserts for new types

--- skia-safe/tests/send_sync.rs
+++ skia-safe/tests/send_sync.rs
@@ -178,6 +178,7 @@ mod gpu {
         assert_not_impl_any!(Interface: Send, Sync);
         assert_impl_all!(TextureInfo: Send, Sync);
         assert_impl_all!(FramebufferInfo: Send, Sync);
+        assert_impl_all!(SurfaceInfo: Send, Sync);
     }
 
     #[cfg(feature = "metal")]
@@ -185,6 +186,7 @@ mod gpu {
         use skia_safe::gpu::mtl::*;
         use static_assertions::*;
         assert_impl_all!(TextureInfo: Send, Sync);
+        assert_impl_all!(SurfaceInfo: Send, Sync);
         assert_impl_all!(BackendContext: Send, Sync);
     }
 
@@ -203,6 +205,7 @@ mod gpu {
         // GetProc could be Send & Sync , but does it make sense (it's already Copy & Clone)
         assert_not_impl_any!(GetProcOf: Send, Sync);
         assert_impl_all!(DrawableInfo: Send, Sync);
+        assert_impl_all!(SurfaceInfo: Send, Sync);
         assert_impl_all!(BackendDrawableInfo: Send, Sync);
         // Note that we can't make most of vk.rs re-export of native Vulkan types Send nor Sync,
         // because they are just re-exports of simple pointers, which already implement
@@ -218,6 +221,7 @@ mod gpu {
         assert_not_impl_any!(BackendContext: Sync);
         assert_impl_all!(TextureResourceInfo: Send, Sync);
         assert_impl_all!(FenceInfo: Send, Sync);
+        assert_impl_all!(SurfaceInfo: Send, Sync);
         assert_impl_all!(Alloc: Send, Sync);
         assert_impl_all!(MemoryAllocator: Send, Sync);
     }
