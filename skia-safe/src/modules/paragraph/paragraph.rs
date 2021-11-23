6d590008caa4b306ce37d6384f4f2f9e5191aca2
Armin Sander <armin@replicator.org>
2021-11-23 10:54:46 -0800

    Add regression test for #585

--- skia-safe/src/modules/paragraph/paragraph.rs
+++ skia-safe/src/modules/paragraph/paragraph.rs
@@ -200,4 +200,24 @@ mod tests {
 
         static LOREM_IPSUM: &str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at leo at nulla tincidunt placerat. Proin eget purus augue. Quisque et est ullamcorper, pellentesque felis nec, pulvinar massa. Aliquam imperdiet, nulla ut dictum euismod, purus dui pulvinar risus, eu suscipit elit neque ac est. Nullam eleifend justo quis placerat ultricies. Vestibulum ut elementum velit. Praesent et dolor sit amet purus bibendum mattis. Aliquam erat volutpat.";
     }
+
+    /// Regression test for <https://github.com/rust-skia/rust-skia/issues/585>
+    #[test]
+    #[serial_test::serial]
+    fn test_style_metrics() {
+        icu::init();
+
+        let mut style = ParagraphStyle::new();
+        let ts = TextStyle::new();
+        style.set_text_style(&ts);
+        let mut font_collection = FontCollection::new();
+        font_collection.set_default_font_manager(FontMgr::default(), None);
+        let mut paragraph_builder = ParagraphBuilder::new(&style, font_collection);
+        paragraph_builder.add_text("Lorem ipsum dolor sit amet\n");
+        let mut paragraph = paragraph_builder.build();
+        paragraph.layout(100.0);
+
+        let line_metrics = &paragraph.get_line_metrics()[0];
+        line_metrics.get_style_metrics(line_metrics.start_index..line_metrics.end_index);
+    }
 }
