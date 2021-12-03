06016bcb82976445ebfb194d14ce9c5852ca1aa4
Roman Petrenko <roman@canva.com>
2021-12-03 15:49:15 +1100

    Fix WOFF2 support on Linux (#34)

--- skia-bindings/build_support/skia/config.rs
+++ skia-bindings/build_support/skia/config.rs
@@ -83,6 +83,7 @@ impl FinalBuildConfiguration {
                 ("skia_use_system_zlib", yes_if(use_system_libraries)),
                 ("skia_use_freetype", yes()),
                 ("skia_use_fonthost_mac", no()),
+                ("skia_use_system_freetype2", no()),
                 ("skia_use_freetype_woff2", yes()),
                 ("skia_use_xps", no()),
                 ("skia_use_dng_sdk", yes_if(features.dng)),
