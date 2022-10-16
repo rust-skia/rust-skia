3190d1d31cc6d017c6c14106216f82abcedaca38
Armin Sander <armin@replicator.org>
2022-10-16 11:51:08 +0200

    m107: Update deprecated attributes

--- skia-safe/src/effects/color_matrix_filter.rs
+++ skia-safe/src/effects/color_matrix_filter.rs
@@ -1,13 +1,13 @@
 use crate::{color_filters, Color, ColorFilter};
 
 impl ColorFilter {
-    #[deprecated(since = "0.0.0", note = "Use color_filters::lighting()")]
+    #[deprecated(since = "0.56.0", note = "Use color_filters::lighting()")]
     pub fn new_lighting(mul: impl Into<Color>, add: impl Into<Color>) -> Option<Self> {
         color_filters::lighting(mul, add)
     }
 }
 
-#[deprecated(since = "0.0.0", note = "Use color_filters::lighting()")]
+#[deprecated(since = "0.56.0", note = "Use color_filters::lighting()")]
 pub fn new_lighting(mul: impl Into<Color>, add: impl Into<Color>) -> Option<ColorFilter> {
     color_filters::lighting(mul, add)
 }
--- skia-safe/src/effects/table_color_filter.rs
+++ skia-safe/src/effects/table_color_filter.rs
@@ -1,12 +1,12 @@
 use crate::{color_filters, ColorFilter};
 
 impl ColorFilter {
-    #[deprecated(since = "0.0.0", note = "Use color_filters::table()")]
+    #[deprecated(since = "0.56.0", note = "Use color_filters::table()")]
     pub fn from_table(table: &[u8; 256]) -> Self {
         color_filters::table(table).unwrap()
     }
 
-    #[deprecated(since = "0.0.0", note = "Use color_filters::table_argb()")]
+    #[deprecated(since = "0.56.0", note = "Use color_filters::table_argb()")]
     pub fn from_argb(
         table_a: Option<&[u8; 256]>,
         table_r: Option<&[u8; 256]>,
@@ -17,12 +17,12 @@ impl ColorFilter {
     }
 }
 
-#[deprecated(since = "0.0.0", note = "Use color_filters::table()")]
+#[deprecated(since = "0.56.0", note = "Use color_filters::table()")]
 pub fn from_table(table: &[u8; 256]) -> ColorFilter {
     color_filters::table(table).unwrap()
 }
 
-#[deprecated(since = "0.0.0", note = "Use color_filters::table_argb()")]
+#[deprecated(since = "0.56.0", note = "Use color_filters::table_argb()")]
 pub fn from_argb(
     table_a: Option<&[u8; 256]>,
     table_r: Option<&[u8; 256]>,
