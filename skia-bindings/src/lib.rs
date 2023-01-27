b299245f69d7730f71db0ae96d24c064a70020e0
Armin Sander <armin@replicator.org>
2023-01-27 15:33:20 +0100

    Move build_supports over to a separate test project

--- skia-bindings/src/lib.rs
+++ skia-bindings/src/lib.rs
@@ -21,17 +21,3 @@ pub mod icu;
 #[doc(hidden)]
 #[cfg(feature = "use-system-jpeg-turbo")]
 use mozjpeg_sys;
-
-#[cfg(test)]
-#[path = "../build_support"]
-mod build_support {
-    pub mod binaries_config;
-    #[cfg(feature = "binary-cache")]
-    pub mod binary_cache;
-    pub mod cargo;
-    pub mod clang;
-    pub mod features;
-    pub mod platform;
-    pub mod skia;
-    pub mod skia_bindgen;
-}
---
+++ skia-bindings/tests/build_support.rs
@@ -0,0 +1,12 @@
+#[path = "../build_support"]
+mod build_support {
+    pub mod binaries_config;
+    #[cfg(feature = "binary-cache")]
+    pub mod binary_cache;
+    pub mod cargo;
+    pub mod clang;
+    pub mod features;
+    pub mod platform;
+    pub mod skia;
+    pub mod skia_bindgen;
+}
