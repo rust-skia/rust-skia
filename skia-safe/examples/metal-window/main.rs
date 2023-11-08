e30e2a65e344b3b9aad6951713bd3f117a0c0604
Armin Sander <armin@replicator.org>
2023-11-08 18:07:38 +0100

    Update metal-window example

--- skia-safe/examples/metal-window/main.rs
+++ skia-safe/examples/metal-window/main.rs
@@ -22,20 +22,26 @@ fn main() {
     use winit::{
         dpi::LogicalSize,
         event::{Event, WindowEvent},
-        event_loop::{ControlFlow, EventLoop},
-        platform::macos::WindowExtMacOS,
+        event_loop::EventLoop,
+        raw_window_handle::HasWindowHandle,
         window::WindowBuilder,
     };
 
     let size = LogicalSize::new(800, 600);
 
-    let events_loop = EventLoop::new();
+    let events_loop = EventLoop::new().expect("Failed to create event loop");
 
     let window = WindowBuilder::new()
         .with_inner_size(size)
         .with_title("Skia Metal Winit Example".to_string())
         .build(&events_loop)
         .unwrap();
+
+    let window_handle = window
+        .window_handle()
+        .expect("Failed to retrieve a window handle");
+
+    let raw_window_handle = window_handle.as_raw();
 
     let device = Device::system_default().expect("no device found");
 
@@ -47,7 +53,10 @@ fn main() {
         layer.set_presents_with_transaction(false);
 
         unsafe {
-            let view = window.ns_view() as cocoa_id;
+            let view = match raw_window_handle {
+                raw_window_handle::RawWindowHandle::AppKit(appkit) => appkit.ns_view.as_ptr(),
+                _ => panic!("Wrong window handle type"),
+            } as cocoa_id;
             view.setWantsLayer(YES);
             view.setLayer(layer.as_ref() as *const _ as _);
         }
@@ -67,61 +76,63 @@ fn main() {
 
     let mut context = DirectContext::new_metal(&backend, None).unwrap();
 
-    events_loop.run(move |event, _, control_flow| {
-        autoreleasepool(|| {
-            *control_flow = ControlFlow::Wait;
+    events_loop
+        .run(move |event, window_target| {
+            autoreleasepool(|| {
+                if let Event::WindowEvent { event, .. } = event {
+                    match event {
+                        WindowEvent::CloseRequested => window_target.exit(),
+                        WindowEvent::Resized(size) => {
+                            metal_layer.set_drawable_size(CGSize::new(
+                                size.width as f64,
+                                size.height as f64,
+                            ));
+                            window.request_redraw()
+                        }
+                        WindowEvent::RedrawRequested => {
+                            if let Some(drawable) = metal_layer.next_drawable() {
+                                let drawable_size = {
+                                    let size = metal_layer.drawable_size();
+                                    Size::new(size.width as scalar, size.height as scalar)
+                                };
 
-            match event {
-                Event::WindowEvent { event, .. } => match event {
-                    WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-                    WindowEvent::Resized(size) => {
-                        metal_layer
-                            .set_drawable_size(CGSize::new(size.width as f64, size.height as f64));
-                        window.request_redraw()
-                    }
-                    _ => (),
-                },
-                Event::RedrawRequested(_) => {
-                    if let Some(drawable) = metal_layer.next_drawable() {
-                        let drawable_size = {
-                            let size = metal_layer.drawable_size();
-                            Size::new(size.width as scalar, size.height as scalar)
-                        };
+                                let mut surface = unsafe {
+                                    let texture_info = mtl::TextureInfo::new(
+                                        drawable.texture().as_ptr() as mtl::Handle,
+                                    );
 
-                        let mut surface = unsafe {
-                            let texture_info =
-                                mtl::TextureInfo::new(drawable.texture().as_ptr() as mtl::Handle);
+                                    let backend_render_target = BackendRenderTarget::new_metal(
+                                        (drawable_size.width as i32, drawable_size.height as i32),
+                                        &texture_info,
+                                    );
 
-                            let backend_render_target = BackendRenderTarget::new_metal(
-                                (drawable_size.width as i32, drawable_size.height as i32),
-                                &texture_info,
-                            );
+                                    gpu::surfaces::wrap_backend_render_target(
+                                        &mut context,
+                                        &backend_render_target,
+                                        SurfaceOrigin::TopLeft,
+                                        ColorType::BGRA8888,
+                                        None,
+                                        None,
+                                    )
+                                    .unwrap()
+                                };
 
-                            gpu::surfaces::wrap_backend_render_target(
-                                &mut context,
-                                &backend_render_target,
-                                SurfaceOrigin::TopLeft,
-                                ColorType::BGRA8888,
-                                None,
-                                None,
-                            )
-                            .unwrap()
-                        };
+                                draw(surface.canvas());
 
-                        draw(surface.canvas());
+                                context.flush_and_submit();
+                                drop(surface);
 
-                        context.flush_and_submit();
-                        drop(surface);
-
-                        let command_buffer = command_queue.new_command_buffer();
-                        command_buffer.present_drawable(drawable);
-                        command_buffer.commit();
+                                let command_buffer = command_queue.new_command_buffer();
+                                command_buffer.present_drawable(drawable);
+                                command_buffer.commit();
+                            }
+                        }
+                        _ => (),
                     }
                 }
-                _ => {}
-            }
-        });
-    });
+            });
+        })
+        .expect("run() failed");
 }
 
 /// Renders a rectangle that occupies exactly half of the canvas
