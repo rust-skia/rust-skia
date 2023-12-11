ae07a30f4eb11ef0927fa666fdbc435387afe05b
namse <skatpgusskat@naver.com>
2023-12-11 18:51:43 +0000

    Change winapi,wio to windows-rs

--- skia-safe/src/gpu/d3d.rs
+++ skia-safe/src/gpu/d3d.rs
@@ -1,5 +1,4 @@
 use skia_bindings as sb;
-use winapi::{shared::dxgi, shared::dxgiformat, um::d3d12};
 
 mod backend_context;
 pub use backend_context::*;
@@ -11,11 +10,11 @@ pub use types::*;
 
 pub use sb::GrD3DResourceStateEnum as ResourceStateEnum;
 
-pub use d3d12::ID3D12CommandQueue;
-pub use d3d12::ID3D12Device;
-pub use d3d12::ID3D12Resource;
-pub use d3d12::D3D12_RESOURCE_STATES;
-pub use dxgi::IDXGIAdapter1;
-pub use dxgiformat::DXGI_FORMAT;
+pub use windows::Win32::Graphics::Direct3D12::ID3D12CommandQueue;
+pub use windows::Win32::Graphics::Direct3D12::ID3D12Device;
+pub use windows::Win32::Graphics::Direct3D12::ID3D12Resource;
+pub use windows::Win32::Graphics::Direct3D12::D3D12_RESOURCE_STATES;
+pub use windows::Win32::Graphics::Dxgi::Common::DXGI_FORMAT;
+pub use windows::Win32::Graphics::Dxgi::IDXGIAdapter1;
 
-native_transmutable!(sb::DXGI_FORMAT, dxgiformat::DXGI_FORMAT, dxgi_format_layout);
+native_transmutable!(sb::DXGI_FORMAT, DXGI_FORMAT, dxgi_format_layout);
