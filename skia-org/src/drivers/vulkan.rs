4b8bd01457ee210b5290c39624b9caf621def352
Armin Sander <armin@replicator.org>
2024-04-06 17:26:13 +0200

    Port skia.org vulkan code to ash-0.38.0

--- skia-org/src/drivers/vulkan.rs
+++ skia-org/src/drivers/vulkan.rs
@@ -100,7 +100,7 @@ impl AshGraphics {
     pub fn vulkan_version() -> Option<(usize, usize, usize)> {
         let entry = unsafe { Entry::load() }.unwrap();
 
-        let detected_version = entry.try_enumerate_instance_version().unwrap_or(None);
+        let detected_version = unsafe { entry.try_enumerate_instance_version().unwrap_or(None) };
 
         detected_version.map(|ver| {
             (
@@ -132,7 +132,7 @@ impl AshGraphics {
             let layer_names: [&CString; 0] = []; // [CString::new("VK_LAYER_LUNARG_standard_validation").unwrap()];
             let extension_names_raw = []; // extension_names();
 
-            let app_info = vk::ApplicationInfo::builder()
+            let app_info = vk::ApplicationInfo::default()
                 .application_name(&app_name)
                 .application_version(0)
                 .engine_name(&app_name)
@@ -144,7 +144,7 @@ impl AshGraphics {
                 .map(|raw_name| raw_name.as_ptr())
                 .collect();
 
-            let create_info = vk::InstanceCreateInfo::builder()
+            let create_info = vk::InstanceCreateInfo::default()
                 .application_info(&app_info)
                 .enabled_layer_names(&layers_names_raw)
                 .enabled_extension_names(&extension_names_raw);
@@ -185,14 +185,13 @@ impl AshGraphics {
 
             let priorities = [1.0];
 
-            let queue_info = [vk::DeviceQueueCreateInfo::builder()
+            let queue_info = [vk::DeviceQueueCreateInfo::default()
                 .queue_family_index(queue_family_index as _)
-                .queue_priorities(&priorities)
-                .build()];
+                .queue_priorities(&priorities)];
 
             let device_extension_names_raw = [];
 
-            let device_create_info = vk::DeviceCreateInfo::builder()
+            let device_create_info = vk::DeviceCreateInfo::default()
                 .queue_create_infos(&queue_info)
                 .enabled_extension_names(&device_extension_names_raw)
                 .enabled_features(&features);
