8ff93512c92a1dbdae8e4a6f90b1b174e6a7d98e
Armin Sander <armin@replicator.org>
2026-01-12 06:25:52 +0100

    gl-window: Add comment for why release_resources_and_abandon() needs to be called on the graphics context

--- skia-safe/examples/gl-window/main.rs
+++ skia-safe/examples/gl-window/main.rs
@@ -220,6 +220,9 @@ fn main() {
 
     impl Drop for Env {
         fn drop(&mut self) {
+            // This fixes a segmentation fault on AMD GPUs, see
+            // <https://github.com/rust-skia/rust-skia/pull/1235> and
+            // <https://github.com/marc2332/freya/issues/347> for more details.
             self.gr_context.release_resources_and_abandon();
         }
     }
