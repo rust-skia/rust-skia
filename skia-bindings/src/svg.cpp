669f01bb897e8523e911f1848596a4bb0c902bfb
Maxime Vincent <maxime@veemax.be>
2026-02-04 12:56:37 +0100

    skottie: add Builder, extract RustResourceProvider

--- skia-bindings/src/svg.cpp
+++ skia-bindings/src/svg.cpp
@@ -1,4 +1,5 @@
 #include "bindings.h"
+#include "rust_resource_provider.h"
 
 #include "include/core/SkCanvas.h"
 #include "include/svg/SkSVGCanvas.h"
@@ -45,77 +46,8 @@
 #include "modules/svg/include/SkSVGTypes.h"
 #include "modules/svg/include/SkSVGUse.h"
 #include "modules/svg/include/SkSVGValue.h"
-#include "modules/skresources/include/SkResources.h"
 
 #include "include/core/SkStream.h"
-
-extern "C" bool C_ImageAsset_isMultiFrame(skresources::ImageAsset* self) {
-    return self->isMultiFrame();
-}
-
-extern "C" skresources::ImageAsset* C_MultiFrameImageAsset_Make(
-    SkData* data, skresources::ImageDecodeStrategy decodeStrategy)
-{
-    return skresources::MultiFrameImageAsset::Make(sp(data), decodeStrategy).release();
-}
-
-namespace ResourceProvider { 
-    extern "C" {
-        typedef void (*Drop)(TraitObject);
-
-        typedef SkData *(*Load)(TraitObject, const char resource_path[], const char resource_name[]);
-        typedef skresources::ImageAsset* (*LoadImageAsset)(TraitObject, const char resource_path[], const char resource_name[], const char resource_id[]);
-        typedef SkTypeface *(*LoadTypeface)(TraitObject, const char name[], const char url[]);
-        typedef SkFontMgr *(*FontMgr)(TraitObject);
-    }
-}
-
-class RustResourceProvider final : public skresources::ResourceProvider {
-public:
-    struct Param {
-        TraitObject trait;
-        ::ResourceProvider::Drop drop;
-        ::ResourceProvider::Load load;
-        ::ResourceProvider::LoadImageAsset loadImageAsset;
-        ::ResourceProvider::LoadTypeface loadTypeface;
-        ::ResourceProvider::FontMgr fontMgr;
-    };
-
-    explicit RustResourceProvider(const Param& param) 
-    : _param(param) 
-    { }
-
-    virtual ~RustResourceProvider() {
-        _param.drop(_param.trait);
-    }
-
-    sk_sp<SkData> load(const char resource_path[], const char resource_name[]) const override {
-        return sp(_param.load(_param.trait, resource_path, resource_name));
-    }
-
-    sk_sp<skresources::ImageAsset> loadImageAsset(
-        const char resource_path[],
-        const char resource_name[],
-        const char resource_id[]) const override {
-        return sp(_param.loadImageAsset(_param.trait, resource_path, resource_name, resource_id));
-    }
-
-    sk_sp<SkTypeface> loadTypeface(const char name[], const char url[]) const override {
-        return sp(_param.loadTypeface(_param.trait, name, url));
-    }
- 
-    // This is here to provide access to the FontMgr to the Dom.
-    sk_sp<SkFontMgr> fontMgr() const {
-        return sp(_param.fontMgr(_param.trait));
-    }
-
-private:
-    Param _param;
-};
-
-extern "C" RustResourceProvider* C_RustResourceProvider_New(const RustResourceProvider::Param* param) {
-    return new RustResourceProvider(*param);
-}
 
 extern "C" SkSVGDOM* C_SkSVGDOM_MakeFromStream(
     SkStream& stream,
